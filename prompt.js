d76952035410d3976a6d79213459eda977102c07
Vansh Prajapati <Vanshprajapati115@gmail.com>
2024-02-03 23:15:36 +0530

    first commit

---
+++ prompt.js
@@ -0,0 +1,38 @@
+// let num= prompt("Enter Number");
+// if(num%5==0){
+//     console.log("Number multiple of 5");
+   
+// }
+// else{
+//     console.log("Not multiple of 5");
+// }
+
+// let str= "Vansh Prajapati      ";
+// console.log(str.toLowerCase());
+// console.log(str.toUpperCase());
+// console.log(str.trim());
+
+
+// let First_Name = prompt("Enter First Name");
+// let first_name=First_Name.toLowerCase();
+// let Last_Name = prompt("Enter Last Name");
+// let last_name=Last_Name.toLowerCase();
+
+// let full_name= first_name.concat(last_name);
+// let len= full_name.length;
+// console.log("@"+full_name.concat(len));
+
+
+// let arr = [1,2,3,4,5];
+// arr.forEach(function sum(val){
+//     let sum = 0;
+//     sum = sum+val;
+//     console.log(sum);
+// });
+
+let arr = [1,2,3,4,5];
+var sum = 0;
+for(let i=0; i<arr.length; i++){
+    sum = sum+arr[i];
+    console.log(sum);
+};
---
+++ script.js
@@ -0,0 +1,13 @@
+let age=18;
+console.log("hello World");
+console.log(age);
+alert("have you any query")
+
+const student = {
+    fullName : "vansh prajapati",
+    age : 17,
+    Batch : 'A3'
+};
+
+student[age]= student[age]+1;
+console.log(student);
